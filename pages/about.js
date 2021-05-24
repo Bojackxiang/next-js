da1b958139c62fc2f0b99ecf595bbd5a41cfb09a
Alex <alex@q-83.com>
2021-05-24 21:08:12 +1000

    day 1 | 添加了 index 和 about 的页面

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+/**
+ * this page will be used as about page
+ */
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      about
+    </div>
+  )
+}
+
+export default about
